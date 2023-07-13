197e3d2ef89b3c9ce2e8c5c2127430428c816950
dsudnr96 <dsudnr96@gmail.com>
2023-07-13 22:12:18 +0900

    jpa-starter

---
+++ src/main/java/com/example/demo/JpaStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaStarterApplication.class, args);
+	}
+
+}
