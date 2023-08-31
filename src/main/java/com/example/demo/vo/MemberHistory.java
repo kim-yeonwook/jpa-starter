8dffc201aa683fcc5dcc86cc3079a0e4881cdfe6
dsudnr96 <dsudnr96@gmail.com>
2023-09-01 08:55:51 +0900

    1

--- src/main/java/com/example/demo/vo/MemberHistory.java
+++ src/main/java/com/example/demo/vo/MemberHistory.java
@@ -1,7 +1,5 @@
 package com.example.demo.vo;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
